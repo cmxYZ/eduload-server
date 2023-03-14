934d5d100da7ee0655e2c202ae21cea4ce921d34
cmxYZ <71211560+cmxYZ@users.noreply.github.com>
2023-03-14 12:28:49 +0500

    add db conn

---
+++ resources/views/loaddata.blade.php
@@ -0,0 +1,50 @@
+
+
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'mon-ser-rtf-123');
+define('DB_NAME', 'eduload');
+
+$connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+mysqli_set_charset($connection, 'utf8');
+if ($connection->connect_error) {
+    die('Error: Database connection failed: ' . $connection->connect_error);
+}
+
+function prepareData($data)
+{
+    global $connection;
+    return mysqli_real_escape_string($connection, stripslashes(htmlspecialchars($data)));
+}
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Keycloak Auth</title>
+    <style>
+        .content {
+            width: 300px;
+            margin: auto;
+            margin-top: 100px;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () { return view('welcome'); } );
-Route::get('/keycloak', function () { return view('keycloak'); } );
+Route::get('/loaddata', function () { return view('loaddata'); } );
