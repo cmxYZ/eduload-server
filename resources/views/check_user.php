1a200625094a0d39dd4e48c230e677934ba189de
cmxYZ <71211560+cmxYZ@users.noreply.github.com>
2023-04-17 08:33:45 +0500

    insert ignore

--- resources/views/check_user.php
+++ resources/views/check_user.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\DB;
     $login = $_GET["login"];
     $id = $_GET["samAccountName"];
 
-    DB::insert("INSERT INTO `Users` (`login`, `roleID`, `samAccountName`, `password`, `isKeycloak`) VALUES ('$login', NULL, '$id', NULL, '1')");
+    DB::insert("INSERT IGNORE INTO `Users` (`login`, `roleID`, `samAccountName`, `password`, `isKeycloak`) VALUES ('$login', NULL, '$id', NULL, '1')");
 //    try {
 //} catch (Exception $e) {
 //}
