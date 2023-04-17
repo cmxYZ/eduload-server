99cf1bf625d892e23562368a9364d4dd3d5f2fc4
cmxYZ <71211560+cmxYZ@users.noreply.github.com>
2023-04-17 09:00:39 +0500

    update data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -13,8 +14,10 @@ class UserController extends Controller
 
     public function load_data()
     {
-        $result = $this->get_result();
-        return view('json', compact('result'));
+        if (!file_exists('data.json')) {
+            $this->updatedatajson();
+        }
+        return file_get_contents('data.json');
     }
 
     public function check_user()
@@ -22,12 +25,53 @@ class UserController extends Controller
         return view('check_user');
     }
 
-    public function get_result()
+    public function updatedatajson()
     {
-        return [
-            'name' => 'John',
-            'tkey' => 't3h54918hkhd9h42h3kh8dyfxk34k',
-        ];
+        $year = "2022";
+        $data = array();
+        $result = DB::select("SELECT * FROM `Teachers`");
+
+        foreach ($result as $row)
+        {
+            $tkey = $row->tkey;
+            $name = $row->firstName . ' ' .  $row->patronymic . ' ' . $row->lastName;
+            $infoWorkPlaces = $row->infoWorkPlaces;
+            $stake = $row->stake ?? '-';
+//            if ($row->stake != null && $row->stake != '')
+//            {
+//                $stake = $row->stake;
+//            }
+
+            $b = $this->SummHours("SELECT plannedHours, realHours FROM `Loads` WHERE tkey='$tkey' AND compensationType='бюджет' AND year='$year'");
+            $c = $this->SummHours("SELECT plannedHours, realHours FROM `Loads` WHERE tkey='$tkey' AND compensationType='контракт' AND year='$year'");
+            $a = $this->SummHours("SELECT plannedHours, realHours FROM `Loads` WHERE tkey='$tkey' AND year='$year'");
+
+            $line = ["name" => "$name", "infoWorkPlaces" => "$infoWorkPlaces", "stake" => "$stake",
+                "hoursOnStake" => "0", "hours" => "0",
+                "bHoursPlaned" => $b[0], "bHoursReal" => $b[1], "bHoursDiff" => $b[2],
+                "cHoursPlaned" => $c[0], "cHoursReal" => $c[1], "cHoursDiff" => $c[2],
+                "hoursPlaned" => $a[0], "hoursReal" => $a[1], "hoursDiff" => $a[2],
+                "year" => $year];
+            array_push($data, $line);
+        }
+        $json = json_encode($data, JSON_UNESCAPED_UNICODE);
+        file_put_contents('data.json', $json);
     }
 
+    public function SummHours($sql)
+    {
+        $real = 0;
+        $planed = 0;
+        $result = DB::select($sql);
+        foreach ($result as $row)
+        {
+            $planed += (float)$row->plannedHours;
+            $real += (float)$row->realHours;
+        }
+        $diff = $planed-$real;
+        $real = round($real, 3);
+        $planed = round($planed, 3);
+        $diff = round($diff, 3);
+        return [$planed, $real, $diff];
+    }
 }
--- resources/views/load_data.php
+++ resources/views/load_data.php
@@ -1,68 +1,11 @@
-<?php 
+<?php
 header('Access-Control-Allow-Origin: *');
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'mon-ser-rtf-123');
-define('DB_NAME', 'eduload');
 
-$connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-mysqli_set_charset($connection, 'utf8');
-if ($connection->connect_error) {
-    die('Error: Database connection failed: ' . $connection->connect_error);
-}
 
-if (!file_exists('data.json')) {
-$year = "2022";
-$data = array();
-$sql = "SELECT * FROM `Teachers`";
-$result = mysqli_query($connection, $sql);
-$result = $result->fetch_all();
 
-foreach ($result as $row)
-{
-    $tkey = $row[0];
-    $name = $row[2] . ' ' .  $row[3] . ' ' . $row[4];
-    $infoWorkPlaces = $row[7];
-    $stake = '-';
-    if ($row[6] != null && $row[6] != '') 
-    {
-        $stake = $row[6];
-    }
 
-    $b = SummHours("SELECT plannedHours, realHours FROM `Loads` WHERE tkey='$tkey' AND compensationType='бюджет' AND year='$year'", $connection);
-    $c = SummHours("SELECT plannedHours, realHours FROM `Loads` WHERE tkey='$tkey' AND compensationType='контракт' AND year='$year'", $connection);
-    $a = SummHours("SELECT plannedHours, realHours FROM `Loads` WHERE tkey='$tkey' AND year='$year'", $connection);
+//$get_data = file_get_contents('data.json');
+//echo $get_data;
 
-    $line = ["name" => "$name", "infoWorkPlaces" => "$infoWorkPlaces", "stake" => "$stake", 
-    "hoursOnStake" => "0", "hours" => "0", 
-    "bHoursPlaned" => $b[0], "bHoursReal" => $b[1], "bHoursDiff" => $b[2], 
-    "cHoursPlaned" => $c[0], "cHoursReal" => $c[1], "cHoursDiff" => $c[2], 
-    "hoursPlaned" => $a[0], "hoursReal" => $a[1], "hoursDiff" => $a[2], 
-    "year" => $year];
-    array_push($data, $line);
-}
-$json = json_encode($data, JSON_UNESCAPED_UNICODE);
-file_put_contents('data.json', $json);
-}
 
-$get_data = file_get_contents('data.json');
-echo $get_data;
-
-function SummHours($sql, $connection)
-{
-    $real = 0;
-    $planed = 0;
-    $result = mysqli_query($connection, $sql);
-    $result = $result->fetch_all();
-    foreach ($result as $row)
-    {
-        $planed += (float)$row[0];
-        $real += (float)$row[1];
-    }
-    $diff = $planed-$real;
-    $real = round($real, 3);
-    $planed = round($planed, 3);
-    $diff = round($diff, 3);
-    return [$planed, $real, $diff];
-}
